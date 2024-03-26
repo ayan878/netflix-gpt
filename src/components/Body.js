ab51ad554902a2d8ac94d0abaf979200b137b518
ayan878 <ayanraza197@gmail.com>
2024-03-27 02:18:15 +0530

    body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,14 @@
-// import React from "react";
-// import NetflixBg from "../assets/netflix-bg.jpg";
-// import Login from "./Login";
-
-// function Body() {
-//   return (
-//     <div>
-//       <img src={NetflixBg} alt="Netflix Background" />
-//       <Login />
-//     </div>
-//   );
-// }
-
-// export default Body;
-
-import { createBrowserRouter } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
-import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -30,10 +20,27 @@ const Body = () => {
     },
   ]);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is sign in
+        const { uid, email, displayName,photoURL } = user;
+        dispatch(addUser({ uid, email, displayName,photoURL }));
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+
+    // Clean up the subscription on unmount
+    return unsubscribe;
+  }, []); // Empty dependency array to run the effect only once on component mount
+
   return (
     <div>
       <RouterProvider router={appRouter} />
     </div>
   );
 };
+
 export default Body;
