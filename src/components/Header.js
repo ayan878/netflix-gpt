ab24af8d947ea22117d6915d79aeabaab8b4eb8e
ayan878 <ayanraza197@gmail.com>
2024-03-27 02:42:17 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import Logo from "../assets/Netflix_Logo_PMS.png"
+import React from "react";
+import Logo from "../assets/Netflix_Logo_PMS.png";
+import ProfileIcon from "../assets/profile-icon.png";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 function Header() {
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
   return (
-    <header className='absolute px-8 py-2 bg-gradient-to-b from-black'>
-      <img  className="w-40  ml-4" src={Logo} alt='Logo' />
-    </header>
+    <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img className="w-40 ml-4" src={Logo} alt="Logo" />
+      {user && (
+        <div className="flex p-2 items-center justify-center space-x-2">
+          <img
+            className="w-10 h-10 rounded-md"
+            src={user?.photoURL || ProfileIcon}
+            alt="profile-icon"
+          />
+          <button className="font-bold text-red-500" onClick={handleSignOut}>
+            Sign out
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
-export default Header
+export default Header;
