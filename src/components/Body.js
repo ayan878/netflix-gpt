a5fd43a0022a995bd46a526c7ca2ff4b50c90fae
ayan878 <ayanraza197@gmail.com>
2024-03-26 13:56:49 +0530

    body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Browse from "./Browse";
+import React from "react";
 import NetflixBg from "../assets/netflix-bg.jpg";
+import Login from "./Login";
 
 function Body() {
   return (
     <div>
-      <img src={NetflixBg} alt="netflicBg" />
-      <Browse />
+      <img src={NetflixBg} alt="Netflix Background" />
+      <Login />
     </div>
   );
 }
 
-export default Body
+export default Body;
