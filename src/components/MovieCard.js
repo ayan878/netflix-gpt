48971c337bd826f1f0052de73524cbc076a1286f
ayan878 <ayanraza197@gmail.com>
2024-03-29 03:35:00 +0530

    enhanced card ui

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,9 +3,14 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4 shadow-2xl">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
+    <div className="w-36 md:w-48 pr-4">
+      <img
+        className="rounded-md"
+        alt="Movie Card"
+        src={IMG_CDN_URL + posterPath}
+      />
     </div>
   );
 };
+
 export default MovieCard;
