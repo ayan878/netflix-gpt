e9bdecf8e088c4d6529cc7e71daba505b19b8df3
ayan878 <ayanraza197@gmail.com>
2024-03-29 02:53:18 +0530

    add multiple custom hooks

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,20 @@
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies"
 import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovie from "../hooks/usePopularMovie";
+import useUpcomingMovie from "../hooks/useUpcomingMovie";
+import useTopRatedMovie from "../hooks/useTopRatedMovie";
 function Browse() {
  useNowPlayingMovies()
+ usePopularMovie()
+ useUpcomingMovie()
+ useTopRatedMovie()
   return (
     <div>
       <Header />
       <MainContainer/>
+      <SecondaryContainer/>
     
       {/* MainCotainer
             - VideoBackgroud
