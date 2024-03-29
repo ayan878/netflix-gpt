cc585c431d4c932cf754e114d9d02df504df362e
ayan878 <ayanraza197@gmail.com>
2024-03-29 23:35:51 +0530

    gptSlice

--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,13 +4,19 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     searchToggle: false,
+    searchExpanded:false,
   },
   reducers: {
     toggleSearch: (state, action) => {
       return { ...state, searchToggle: !state.searchToggle };
     },
+    searchExpanded:(state,action)=>{
+        return {
+            ...state,searchExpanded:!state.searchExpanded
+        }
+    }
   },
 });
 
-export const { toggleSearch } = gptSlice.actions;
+export const { toggleSearch,searchExpanded } = gptSlice.actions;
 export default gptSlice.reducer;
