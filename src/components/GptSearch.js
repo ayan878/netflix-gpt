5196a7da99c471063cdb851b8ed83933d6ed0840
ayan878 <ayanraza197@gmail.com>
2024-03-31 01:51:09 +0530

    add other catogory list

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -15,10 +15,10 @@ if (!movies) return null;
       <div className="w-screen">
         <div className="pt-[30%] md:pt-0  pl-4  md:pl-12 relative z-20">
           <MovieList title={"Search Results"} movies={movies.moviesSearch} />
-          {/* <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+          <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} /> */}
+          <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
         </div>
       </div>
     )
