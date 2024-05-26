cd9e4606d10f5b87bba829933daa7cc1b87fb7f8
ayan878 <ayanraza197@gmail.com>
2024-05-26 23:00:15 +0530

    Movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,12 +2,12 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
   return (
-    <div className="px-6 ">
+    <div className="px-6">
       <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
       <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex">
           {movies?.results.map((movie) => (
-            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+            <MovieCard key={movie.id} posterPath={movie.poster_path} id={movie.id} movie={movie}  />
           ))}
         </div>
       </div>
