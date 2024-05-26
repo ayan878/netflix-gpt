c73613bf6fcc8dabe630d674189a3e1851a6d915
ayan878 <ayanraza197@gmail.com>
2024-05-26 23:01:49 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../assets/Netflix_Logo_PMS.png";
 import { avatar } from "../utils/constants";
 import { auth } from "../utils/firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-// import GptSearchBar from "./GptSearchBar";
 import SearchBar from "./SearchBar";
 
 function Header() {
   const user = useSelector((store) => store.user);
-
   const navigate = useNavigate();
+  const { id } = useParams();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -27,10 +29,13 @@ function Header() {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is sign in
+        // User is signed in
         const { uid, email, displayName, photoURL } = user;
         dispatch(addUser({ uid, email, displayName, photoURL }));
         navigate("/browse");
+        if (location.pathname.startsWith("/browse/movie/")) {
+          navigate(`/browse/movie/${id}`);
+        }
       } else {
         // User is signed out
         dispatch(removeUser());
@@ -38,25 +43,30 @@ function Header() {
       }
     });
 
-    // Clean up the subscription on unmount or unsubscribe when component is unmount
+    // Clean up the subscription on unmount or unsubscribe when component is unmounted
     return unsubscribe;
   }, []); // Empty dependency array to run the effect only once on component mount
 
   return (
-    <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img className="w-40 ml-4" src={Logo} alt="Logo" />
+    <div className="w-full fixed top-0 left-0 px-4 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between items-center">
+      <div className="w-full md:w-auto flex justify-center md:justify-start items-center">
+        <img className="w-28 sm:w-40 ml-2 sm:ml-4" src={Logo} alt="Logo" />
+      </div>
       {user && (
-        <div className="flex items-center justify-center  space-x-2">
-          <SearchBar/>
-
-          <img
-            className="w-10 h-10 rounded-md"
-            src={user?.photoURL || avatar}
-            alt="profile-icon"
-          />
-          <button className="font-bold text-red-500" onClick={handleSignOut}>
-            Sign out
-          </button>
+        <div className="w-full md:w-auto flex flex-col md:flex-row items-center space-x-2 mt-2 md:mt-0">
+          <div className="w-full flex justify-center md:justify-end">
+            <SearchBar isOpen={isSearchOpen} setIsOpen={setIsSearchOpen} />
+          </div>
+          <div className="flex items-center space-x-2">
+            <img
+              className="w-8 h-8 sm:w-10 sm:h-10 rounded-md"
+              src={user?.photoURL || avatar}
+              alt="profile-icon"
+            />
+            <button className="font-bold text-red-500" onClick={handleSignOut}>
+              Sign out
+            </button>
+          </div>
         </div>
       )}
     </div>
