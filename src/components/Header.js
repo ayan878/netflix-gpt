461f0d7e426e34e12e7b992686f8d5ef5b45fabb
ayan878 <ayanraza197@gmail.com>
2024-05-27 01:06:45 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Logo from "../assets/Netflix_Logo_PMS.png";
 import { avatar } from "../utils/constants";
 import { auth } from "../utils/firebase";
@@ -14,7 +14,6 @@ function Header() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const location = useLocation();
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -48,25 +47,21 @@ function Header() {
   }, []); // Empty dependency array to run the effect only once on component mount
 
   return (
-    <div className="w-full fixed top-0 left-0 px-4 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between items-center">
-      <div className="w-full md:w-auto flex justify-center md:justify-start items-center">
-        <img className="w-28 sm:w-40 ml-2 sm:ml-4" src={Logo} alt="Logo" />
-      </div>
+    <div className="w-full fixed top-0 left-0 px-4 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
+      <img className="w-28 sm:w-40 ml-2 sm:ml-4" src={Logo} alt="Logo" />
       {user && (
-        <div className="w-full md:w-auto flex flex-col md:flex-row items-center space-x-2 mt-2 md:mt-0">
-          <div className="w-full flex justify-center md:justify-end">
-            <SearchBar isOpen={isSearchOpen} setIsOpen={setIsSearchOpen} />
-          </div>
-          <div className="flex items-center space-x-2">
-            <img
-              className="w-8 h-8 sm:w-10 sm:h-10 rounded-md"
-              src={user?.photoURL || avatar}
-              alt="profile-icon"
-            />
-            <button className="font-bold text-red-500" onClick={handleSignOut}>
-              Sign out
-            </button>
-          </div>
+        <div className="flex items-center space-x-2 ml-auto">
+          {" "}
+          {/* ml-auto to push logout button to the right */}
+          <SearchBar />
+          <img
+            className="w-8 h-8 sm:w-10 sm:h-10 rounded-md"
+            src={user?.photoURL || avatar}
+            alt="profile-icon"
+          />
+          <button className="font-bold text-red-500" onClick={handleSignOut}>
+            Sign out
+          </button>
         </div>
       )}
     </div>
