ee110e18e36c0eb0a31ee60786d9dd3fa120bbd6
ayan878 <ayanraza197@gmail.com>
2024-05-27 01:07:06 +0530

    movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,15 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { IMG_CDN_URL } from "../utils/constants";
-const MovieCard = ({ id,posterPath }) => {
-  
+
+const MovieCard = ({ id, posterPath }) => {
   if (!posterPath) return null;
+
   return (
     <Link to={{ pathname: `/browse/movie/${id}` }}>
       <div className="w-36 md:w-48 m-2">
         <img
-          className=" rounded-2xl"
+          className="w-full h-auto rounded-2xl"
           alt="Movie Card"
           src={IMG_CDN_URL + posterPath}
         />
